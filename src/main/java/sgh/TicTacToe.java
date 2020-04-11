3101b364872bb0120a7be37db9ed9ae2387d51cb
piotrprusik <61974584+piotrprusik@users.noreply.github.com>
2020-04-11 14:07:42 +0200

    Update TicTacToe.java

--- src/main/java/sgh/TicTacToe.java
+++ src/main/java/sgh/TicTacToe.java
@@ -87,7 +87,7 @@ public class TicTacToe {
 
         return Result.NO_WINNER;
     }
-
+    
     public static void main(String[] args) throws FileNotFoundException {
         Result res = checkBoard("boards/tick0.csv");
         System.out.println(res);
